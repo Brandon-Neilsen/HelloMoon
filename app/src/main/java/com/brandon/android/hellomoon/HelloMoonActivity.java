050fc451158313dd54750ee20655063afac2f4df
Brandon Neilsen <brandon.e.neilsen@gmail.com>
2015-11-11 20:40:18 -0800

    HelloMoon Version 1.4
    Listing 13.5 complete

--- app/src/main/java/com/brandon/android/hellomoon/HelloMoonActivity.java
+++ app/src/main/java/com/brandon/android/hellomoon/HelloMoonActivity.java
@@ -1,11 +1,11 @@
 package com.brandon.android.hellomoon;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class HelloMoonActivity extends AppCompatActivity {
+public class HelloMoonActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
